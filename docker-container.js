2e152b666ca1e4ca535c6c80cdd440ac774f5ec7
Moses Ette <senatoette@gmail.com>
2026-06-12 14:59:48 +0100

    fixing for github assets

---
+++ docker-container.js
@@ -0,0 +1,21 @@
+# Base image
+FROM node:18
+
+# Set working directory
+WORKDIR /app
+
+# Copy files into container
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy rest of the app
+COPY . .
+
+# Expose port
+EXPOSE 3000
+
+# Start app
+CMD ["npm", "start"]
+``
